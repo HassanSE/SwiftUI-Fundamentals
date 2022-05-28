c9a10f70d736873f7e3cde36bcb12a0ee8c4c94e
Muhammad Hassan <hassankhll@hotmail.com>
2022-05-28 10:34:09 +0500

    Add Apple-Framework project

---
+++ Apple-Frameworks/Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Muhammad Hassan on 27/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
