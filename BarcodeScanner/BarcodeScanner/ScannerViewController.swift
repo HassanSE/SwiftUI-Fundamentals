9caad05c9dd03580b3ce53879ee0684343d88164
Muhammad Hassan <hassankhll@hotmail.com>
2022-06-03 21:49:20 +0500

    Setup AVCaptureSession and metadata delegate for ScannerViewController

--- BarcodeScanner/BarcodeScanner/ScannerViewController.swift
+++ BarcodeScanner/BarcodeScanner/ScannerViewController.swift
@@ -16,5 +16,41 @@ protocol ScannerViewControllerDelegate: AnyObject {
 final class ScannerViewController: UIViewController {
     let captureSession = AVCaptureSession()
     var previewLayer: AVCaptureVideoPreviewLayer?
-    weak var delegate: ScannerViewControllerDelegate?
+    weak var scannerDelegate: ScannerViewControllerDelegate?
+    
+    private func setupCaptureSession() {
+        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else { return }
+        let videoInput: AVCaptureDeviceInput
+        do {
+            try videoInput = AVCaptureDeviceInput(device: videoCaptureDevice)
+        } catch {
+            return
+        }
+        if captureSession.canAddInput(videoInput) {
+            captureSession.addInput(videoInput)
+        } else {
+            return
+        }
+        let metaDataOutput = AVCaptureMetadataOutput()
+        if captureSession.canAddOutput(metaDataOutput) {
+            captureSession.addOutput(metaDataOutput)
+            metaDataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            metaDataOutput.metadataObjectTypes = [.ean8, .ean13]
+        } else {
+            return
+        }
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer?.videoGravity = .resizeAspectFill
+        view.layer.addSublayer(previewLayer!)
+        captureSession.startRunning()
+    }
+}
+
+extension ScannerViewController: AVCaptureMetadataOutputObjectsDelegate {
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        guard let object = metadataObjects.first else { return }
+        guard let machineReadableObject = object as? AVMetadataMachineReadableCodeObject else { return }
+        guard let barcode = machineReadableObject.stringValue else { return }
+        scannerDelegate?.didScan(barcode: barcode)
+    }
 }
