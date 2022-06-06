31718a8669bcccea75ebc8fd0cc0446ba462fd94
Muhammad Hassan <hassankhll@hotmail.com>
2022-06-06 21:17:09 +0500

    Add Appetizers project

---
+++ Appetizers/Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Muhammad Hassan on 06/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
