a063a97fbf16b5e14e458eda4d7fa269d4e7f47b
Muhammad Hassan <hassankhll@hotmail.com>
2022-06-07 22:35:37 +0500

    - Add sample appetizer image - Add MockData struct - Add basic List on AppetizerListView

--- Appetizers/Appetizers/Appetizer.swift
+++ Appetizers/Appetizers/Appetizer.swift
@@ -7,12 +7,12 @@
 
 import Foundation
 
-struct Appetizer {
+struct Appetizer: Decodable, Identifiable {
     let id: Int
     let name: String
     let description: String
     let price: Double
-    let imageURL: URL
+    let imageURL: String
     let calories: Int
     let protein: Int
     let carbs: Int
@@ -20,4 +20,17 @@ struct Appetizer {
 
 struct AppetizerResponse {
     let request: [Appetizer]
+}
+
+struct MockData {
+    static let sampleAppetizer = Appetizer(id: 1,
+                                           name: "Test Appetizer",
+                                           description: "This is the description for my appetizer. It's yummy",
+                                           price: 9.99,
+                                           imageURL: "",
+                                           calories: 99,
+                                           protein: 99,
+                                           carbs: 399)
+    
+    static let appetizers = [sampleAppetizer, sampleAppetizer, sampleAppetizer, sampleAppetizer]
 }
