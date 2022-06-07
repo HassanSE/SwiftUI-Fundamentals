1c9d88f849b76732078d30922506a332ee9cdab3
Muhammad Hassan <hassankhll@hotmail.com>
2022-06-07 22:49:08 +0500

    Add AppetizerListCell

---
+++ Appetizers/Appetizers/AppetizerListCell.swift
@@ -0,0 +1,36 @@
+//
+//  AppetizerListCell.swift
+//  Appetizers
+//
+//  Created by Muhammad Hassan on 07/06/2022.
+//
+
+import SwiftUI
+
+struct AppetizerListCell: View {
+    let appetizer: Appetizer
+    var body: some View {
+        HStack {
+            Image("asian-flank-steak")
+                .resizable()
+                .frame(width: 120, height: 90)
+                .aspectRatio(contentMode: .fill)
+                .cornerRadius(8)
+            VStack(alignment: .leading, spacing: 5) {
+                Text(appetizer.name)
+                    .font(.title2)
+                    .fontWeight(.medium)
+                Text("$\(appetizer.price, specifier: "%0.2f")")
+                    .foregroundColor(.secondary)
+                    .fontWeight(.semibold)
+            }
+            .padding(.leading)
+        }
+    }
+}
+
+struct AppetizerListCell_Previews: PreviewProvider {
+    static var previews: some View {
+        AppetizerListCell(appetizer: MockData.sampleAppetizer)
+    }
+}
