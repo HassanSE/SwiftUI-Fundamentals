8f5cba28cc5040b65e5b0b68929b4d6f4685d512
Muhammad Hassan <hassankhll@hotmail.com>
2022-06-07 23:25:02 +0500

    - Add a singleton based NetworkManager - Add getAppetizers(completed:) method on NetworkManager

---
+++ Appetizers/Appetizers/APError.swift
@@ -0,0 +1,15 @@
+//
+//  APError.swift
+//  Appetizers
+//
+//  Created by Muhammad Hassan on 07/06/2022.
+//
+
+import Foundation
+
+enum APError: Error {
+    case invalidURL
+    case invalidResponse
+    case invalidData
+    case unableToComplete
+}
