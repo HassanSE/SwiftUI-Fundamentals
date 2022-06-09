fcbd0a9fee1e1712495bcb4cd4f2e731c1765fe0
Muhammad Hassan <hassankhll@hotmail.com>
2022-06-09 23:08:56 +0500

    Add failure case for getAppetizer network call and add error handling

--- Appetizers/Appetizers/AppetizerListViewModel.swift
+++ Appetizers/Appetizers/AppetizerListViewModel.swift
@@ -9,15 +9,25 @@ import Foundation
 
 final class AppetizerListViewModel: ObservableObject {
     @Published var appetizers: [Appetizer] = []
+    @Published var alertItem: AlertItem?
     
     func getAppetizers() {
-        NetworkManager.shared.getAppetizers { result in
+        NetworkManager.shared.getAppetizers { [self] result in
             DispatchQueue.main.async {
                 switch result {
                 case .success(let appetizers):
                     self.appetizers = appetizers
-                default:
-                    break
+                case .failure(let error):
+                    switch error {
+                    case .invalidData:
+                        self.alertItem = AlertContext.invalidData
+                    case .invalidResponse:
+                        self.alertItem = AlertContext.invalidResponse
+                    case .invalidURL:
+                        self.alertItem = AlertContext.invalidURL
+                    case .unableToComplete:
+                        self.alertItem = AlertContext.unableToComplete
+                    }
                 }
             }
         }
